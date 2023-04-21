2688e45098f93ef7358f127a20df0f838944dcdf
teotoro <toronjadze.teona@gmail.com>
2023-04-22 00:16:01 +0400

    class name correction
    Correction: doubled class name

--- main.js
+++ main.js
@@ -32,10 +32,10 @@ function showSlides(n) {
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active-dot", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " active-dot";
 }
 
 function openTab(evt, tabName) {
