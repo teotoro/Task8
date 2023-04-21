11cb9652300447ad7c92e748260059ff59b06607
teotoro <toronjadze.teona@gmail.com>
2023-04-21 23:41:19 +0400

    single page

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ document.addEventListener('DOMContentLoaded', () => {
     hamburgerMenu.addEventListener('click', function () {
         openDropDownMenu.classList.toggle('active-icon');
         closeDropDownMenu.classList.toggle('active-icon');
-        dropDown.classList.toggle('drop-down')
+        dropDown.classList.toggle('drop-down');
     });
 })
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tab-info");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tab-title");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " active-tab";
+
+    evt.preventDefault()
+  }
+  
+  // first tab default
+  document.getElementById("default").click();
